c4f4b3c028e77918b429121ab5daec3314826b8d
alemarcar5 <alemarcar5@alu.edu.gva.es>
2025-10-27 12:26:46 +0100

    a

---
+++ UD4/src/main/java/Pruebas/Main.java
@@ -0,0 +1,14 @@
+package Pruebas;
+
+public class Main {
+    public static void main(String[] args) {
+        float n1 = 2;
+        float cuadrado = n1 * n1;
+        System.out.printf("El cuadrado es: %1$f es %2$.2f.n", cuadrado);
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
