b714418418a0b27b29afda49390ba9f8426136f7
alemarcar5 <alemarcar5@alu.edu.gva.es>
2025-11-06 13:49:24 +0100

    6/11/2005

--- UD4/src/main/java/T5/EjericioExtra/EJ8.java
+++ UD4/src/main/java/T5/EjericioExtra/EJ8.java
@@ -9,5 +9,41 @@ public class EJ8 {
         int dia = sc.nextInt();
         int mes = sc.nextInt();
 
+        String Horoscopo = "";
+
+        if (mes == 1 && dia <= 19){
+            Horoscopo = "Capricornio";
+        }
+
+        else if (mes == 1 && dia >= 20){
+            Horoscopo = "Acuario";
+        }
+
+        else if (mes == 2 && dia <= 18){
+            Horoscopo = "Aquario";
+        }
+
+        else if (mes == 2 && dia >= 19){
+            Horoscopo = "Piscis";
+        }
+
+        else if (mes == 3 && dia <= 20){
+            Horoscopo = "Piscis";
+        }
+        else if (mes == 3 && dia >= 21){
+            Horoscopo = "Aries";
+        }
+
+        else if (mes == 4 && dia <= 19){
+            Horoscopo = "Aries";
+        }
+        else if (mes == 4 && dia >= 20){
+            Horoscopo = "Tauro";
+        }
+
+        else if (mes == 5 && dia <= 19){}
+
+
+
     }
 }
