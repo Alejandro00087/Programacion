036e0cd20b69698f11967cd567f473aab9ad400a
alemarcar5 <alemarcar5@alu.edu.gva.es>
2025-11-07 10:18:45 +0100

    7/11/2005

--- UD4/src/main/java/T5/EjericioExtra/EJ8.java
+++ UD4/src/main/java/T5/EjericioExtra/EJ8.java
@@ -5,45 +5,88 @@ public class EJ8 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dime que dia y que mes naciste en formato numero");
+        System.out.println("Dime qué día y qué mes naciste en formato número:");
         int dia = sc.nextInt();
         int mes = sc.nextInt();
 
         String Horoscopo = "";
 
-        if (mes == 1 && dia <= 19){
+        if (mes == 1 && dia <= 19) {
             Horoscopo = "Capricornio";
         }
-
-        else if (mes == 1 && dia >= 20){
+        else if (mes == 1 && dia >= 20) {
             Horoscopo = "Acuario";
         }
-
-        else if (mes == 2 && dia <= 18){
-            Horoscopo = "Aquario";
+        else if (mes == 2 && dia <= 18) {
+            Horoscopo = "Acuario";
         }
-
-        else if (mes == 2 && dia >= 19){
+        else if (mes == 2 && dia >= 19) {
             Horoscopo = "Piscis";
         }
-
-        else if (mes == 3 && dia <= 20){
+        else if (mes == 3 && dia <= 20) {
             Horoscopo = "Piscis";
         }
-        else if (mes == 3 && dia >= 21){
+        else if (mes == 3 && dia >= 21) {
             Horoscopo = "Aries";
         }
-
-        else if (mes == 4 && dia <= 19){
+        else if (mes == 4 && dia <= 19) {
             Horoscopo = "Aries";
         }
-        else if (mes == 4 && dia >= 20){
+        else if (mes == 4 && dia >= 20) {
             Horoscopo = "Tauro";
         }
+        else if (mes == 5 && dia <= 20) {
+            Horoscopo = "Tauro";
+        }
+        else if (mes == 5 && dia >= 21) {
+            Horoscopo = "Géminis";
+        }
+        else if (mes == 6 && dia <= 20) {
+            Horoscopo = "Géminis";
+        }
+        else if (mes == 6 && dia >= 21) {
+            Horoscopo = "Cáncer";
+        }
+        else if (mes == 7 && dia <= 22) {
+            Horoscopo = "Cáncer";
+        }
+        else if (mes == 7 && dia >= 23) {
+            Horoscopo = "Leo";
+        }
+        else if (mes == 8 && dia <= 22) {
+            Horoscopo = "Leo";
+        }
+        else if (mes == 8 && dia >= 23) {
+            Horoscopo = "Virgo";
+        }
+        else if (mes == 9 && dia <= 22) {
+            Horoscopo = "Virgo";
+        }
+        else if (mes == 9 && dia >= 23) {
+            Horoscopo = "Libra";
+        }
+        else if (mes == 10 && dia <= 22) {
+            Horoscopo = "Libra";
+        }
+        else if (mes == 10 && dia >= 23) {
+            Horoscopo = "Escorpio";
+        }
+        else if (mes == 11 && dia <= 21) {
+            Horoscopo = "Escorpio";
+        }
+        else if (mes == 11 && dia >= 22) {
+            Horoscopo = "Sagitario";
+        }
+        else if (mes == 12 && dia <= 21) {
+            Horoscopo = "Sagitario";
+        }
+        else if (mes == 12 && dia >= 22) {
+            Horoscopo = "Capricornio";
+        }
+        else {
+            Horoscopo = "Fecha no válida";
+        }
 
-        else if (mes == 5 && dia <= 19){}
-
-
-
+        System.out.println("Tu horóscopo es: " + Horoscopo);
     }
 }
